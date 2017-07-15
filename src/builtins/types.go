18660a7cd85140a22a83b5a0172a7903983e732f
Lee Keitel <lee@keitel.xyz>
2017-07-14 20:11:09 -0500

    Added toString(), to_int -> toInt, to_float -> toFloat
    Since readline() requires a string, thought it'd be nice to
    have a conversion function for types to strings. All types
    except arrays, maps, and functions are valid input and will
    be converted to a string representation.
    
    Renamed to_int and to_float to match toString and because I
    don't actually like underscores in functions names.

--- src/builtins/types.go
+++ src/builtins/types.go
@@ -1,13 +1,16 @@
 package eval
 
 import (
+	"strconv"
+
 	"github.com/nitrogen-lang/nitrogen/src/eval"
 	"github.com/nitrogen-lang/nitrogen/src/object"
 )
 
 func init() {
-	eval.RegisterBuiltin("to_int", toIntBuiltin)
-	eval.RegisterBuiltin("to_float", toFloatBuiltin)
+	eval.RegisterBuiltin("toInt", toIntBuiltin)
+	eval.RegisterBuiltin("toFloat", toFloatBuiltin)
+	eval.RegisterBuiltin("toString", toStringBuiltin)
 
 	eval.RegisterBuiltin("isFloat", makeIsTypeBuiltin(object.FLOAT_OBJ))
 	eval.RegisterBuiltin("isInt", makeIsTypeBuiltin(object.INTEGER_OBJ))
@@ -17,6 +20,8 @@ func init() {
 	eval.RegisterBuiltin("isString", makeIsTypeBuiltin(object.STRING_OBJ))
 	eval.RegisterBuiltin("isArray", makeIsTypeBuiltin(object.ARRAY_OBJ))
 	eval.RegisterBuiltin("isMap", makeIsTypeBuiltin(object.HASH_OBJ))
+	// The below function is a placeholder for later
+	// eval.RegisterBuiltin("isError", makeIsTypeBuiltin(object.ERROR_OBJ))
 }
 
 func toIntBuiltin(env *object.Environment, args ...object.Object) object.Object {
@@ -31,7 +36,7 @@ func toIntBuiltin(env *object.Environment, args ...object.Object) object.Object 
 		return &object.Integer{Value: int64(arg.Value)}
 	}
 
-	return object.NewError("Argument to `to_int` must be FLOAT or INT, got %s", args[0].Type())
+	return object.NewError("Argument to `toInt` must be FLOAT or INT, got %s", args[0].Type())
 }
 
 func toFloatBuiltin(env *object.Environment, args ...object.Object) object.Object {
@@ -46,7 +51,7 @@ func toFloatBuiltin(env *object.Environment, args ...object.Object) object.Objec
 		return arg
 	}
 
-	return object.NewError("Argument to `to_float` must be FLOAT or INT, got %s", args[0].Type())
+	return object.NewError("Argument to `toFloat` must be FLOAT or INT, got %s", args[0].Type())
 }
 
 func makeIsTypeBuiltin(t object.ObjectType) object.BuiltinFunction {
@@ -57,4 +62,27 @@ func makeIsTypeBuiltin(t object.ObjectType) object.BuiltinFunction {
 
 		return object.NativeBoolToBooleanObj(args[0].Type() == t)
 	}
+}
+
+func toStringBuiltin(env *object.Environment, args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return object.NewError("to_string expects 1 argument. Got %d", len(args))
+	}
+
+	converted := ""
+
+	switch arg := args[0].(type) {
+	case *object.String:
+		converted = arg.Value
+	case *object.Float:
+		converted = strconv.FormatFloat(arg.Value, 'G', -1, 64)
+	case *object.Integer:
+		converted = strconv.FormatInt(arg.Value, 10)
+	case *object.Boolean:
+		converted = strconv.FormatBool(arg.Value)
+	case *object.Null:
+		converted = "nil"
+	}
+
+	return &object.String{Value: converted}
 }
--- src/builtins/types_test.go
+++ src/builtins/types_test.go
@@ -2,6 +2,8 @@ package eval
 
 import (
 	"testing"
+
+	"github.com/nitrogen-lang/nitrogen/src/object"
 )
 
 func TestBuiltinIntConvFunction(t *testing.T) {
@@ -9,10 +11,10 @@ func TestBuiltinIntConvFunction(t *testing.T) {
 		input    string
 		expected interface{}
 	}{
-		{`to_int(23.5)`, 23},
-		{`to_int(1)`, 1},
-		{`to_int("hello world")`, "Argument to `to_int` must be FLOAT or INT, got STRING"},
-		{`to_int([])`, "Argument to `to_int` must be FLOAT or INT, got ARRAY"},
+		{`toInt(23.5)`, 23},
+		{`toInt(1)`, 1},
+		{`toInt("hello world")`, "Argument to `toInt` must be FLOAT or INT, got STRING"},
+		{`toInt([])`, "Argument to `toInt` must be FLOAT or INT, got ARRAY"},
 	}
 
 	for _, tt := range tests {
@@ -25,10 +27,10 @@ func TestBuiltinFloatConvFunction(t *testing.T) {
 		input    string
 		expected interface{}
 	}{
-		{`to_float(23.5)`, 23.5},
-		{`to_float(1)`, 1.0},
-		{`to_float("hello world")`, "Argument to `to_float` must be FLOAT or INT, got STRING"},
-		{`to_float([])`, "Argument to `to_float` must be FLOAT or INT, got ARRAY"},
+		{`toFloat(23.5)`, 23.5},
+		{`toFloat(1)`, 1.0},
+		{`toFloat("hello world")`, "Argument to `toFloat` must be FLOAT or INT, got STRING"},
+		{`toFloat([])`, "Argument to `toFloat` must be FLOAT or INT, got ARRAY"},
 	}
 
 	for _, tt := range tests {
@@ -61,5 +63,34 @@ func TestIsBuiltins(t *testing.T) {
 
 	for _, tt := range tests {
 		testLiteralErrorObjects(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestToStringBuiltin(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`toString(5)`, "5"},
+		{`toString(5.6)`, "5.6"},
+		{`toString("Hello")`, "Hello"},
+		{`toString(true)`, "true"},
+		{`toString(false)`, "false"},
+		{`toString(nil)`, "nil"},
+		{`toString([1, 2, 3])`, ""},
+		{`toString({"key": "value"})`, ""},
+	}
+
+	for i, tt := range tests {
+		evaled := testEval(tt.input)
+
+		str, ok := evaled.(*object.String)
+		if !ok {
+			t.Fatalf("%d: Expected string, got %#v", i+1, evaled)
+		}
+
+		if str.Value != tt.expected {
+			t.Fatalf("%d: toString failed. Expected `%s`, got `%s`", i+1, tt.expected, str.Value)
+		}
 	}
 }
