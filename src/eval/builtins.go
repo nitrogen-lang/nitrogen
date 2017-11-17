b8b72091018c3608d16db1dc828f46f0b41455da
Lee Keitel <lee@keitel.xyz>
2017-11-16 21:55:00 -0600

    Check builtin functions for valid identifiers

--- src/eval/builtins.go
+++ src/eval/builtins.go
@@ -1,18 +1,32 @@
 package eval
 
 import (
+	"regexp"
+
 	"github.com/nitrogen-lang/nitrogen/src/object"
 )
 
-var builtins = map[string]*object.Builtin{}
+var (
+	builtins   = map[string]*object.Builtin{}
+	identRegex = regexp.MustCompile(`[a-zA-Z_][a-zA-Z0-9_]*`)
+)
 
+// RegisterBuiltin allows other packages to register functions for availability in user code
 func RegisterBuiltin(name string, fn object.BuiltinFunction) {
+	if !validBuiltinIdent(name) {
+		panic("Invalid builtin function name " + name)
+	}
+
 	if _, defined := builtins[name]; defined {
 		// Panic because this should NEVER happen when built
 		panic("Builtin function " + name + " already defined")
 	}
 
 	builtins[name] = &object.Builtin{Fn: fn}
+}
+
+func validBuiltinIdent(ident string) bool {
+	return identRegex.Match([]byte(ident))
 }
 
 func getBuiltin(name string) object.Object {
