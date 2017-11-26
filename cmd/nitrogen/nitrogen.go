baafc3c4de0756d4f1b70efee2359a10daa43b0b
Lee Keitel <lee@keitel.xyz>
2017-11-26 13:50:03 -0600

    Only show execution time when debugging

--- cmd/nitrogen/nitrogen.go
+++ cmd/nitrogen/nitrogen.go
@@ -110,7 +110,9 @@ func main() {
 		})
 		start := time.Now()
 		result := machine.Execute(code)
-		fmt.Printf("Execution took %s\n", time.Now().Sub(start))
+		if fullDebug {
+			fmt.Printf("Execution took %s\n", time.Now().Sub(start))
+		}
 		if result != nil && result != object.NullConst {
 			if e, ok := result.(*object.Exception); ok {
 				os.Stdout.WriteString("Uncaught Exception: ")
